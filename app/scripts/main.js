d0dcfeee26afeab3a4077c1e0691e47de74e478b
Dezmond Finney <dezmondf@gmail.com>
2012-09-25 16:13:53 -0400

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,49 @@
+function baseLineHeight(element) {
+  var elementHeight, baseLineMeasure, baseLine, baseLineCount, adjustedHeight, elementChild;
+  
+  // elementChild = $(element).find("img").first();
+  $(element).each(function(){
+    elementHeight = $(this).height(); 
+    elementChild = $(this).find("img")
+    elementChildHeight = $(this).find("img").height();
+    baseLineMeasure = $(this).css('line-height');
+    baseLine = parseInt(baseLineMeasure);
+    baseLineCount = Math.ceil(elementChildHeight/baseLine);
+    adjustedHeight = baseLineCount*baseLine;
+    $(this).height(adjustedHeight);
+    
+    console.log(elementChild);
+    console.log("container line height: " + baseLine);
+    console.log("container element height: " + elementHeight);
+    console.log("container image height: " + elementChildHeight);
+    console.log("container readjusted height: " + adjustedHeight);
+    console.log("-----------");
+  });
+}
+
+$.event.special.debouncedresize.threshold = 150;
+
+// debulked onresize handler
+// function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100)};return c};
+
+$(document).ready(function(){
+
+  $("#menu-button").click(
+    function(event){
+      $("#menu").toggleClass("hidden");
+      event.preventDefault();
+    }
+  );
+
+});
+
+$(window).bind('load', function(){
+  
+  $(window).on("debouncedresize", function(){
+    // baseLineHeight("#featured-projects-wrapper");
+    baseLineHeight("#featured-projects-wrapper, p.image");
+  });
+  baseLineHeight("#featured-projects-wrapper, p.image");
+
+});
+
