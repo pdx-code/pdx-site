bc1f1e4c6cdf2743dfb1c01b4f2fcbe626a75ddc
Dezmond Finney <dezmondfinney.pdx@gmail.com>
2012-10-19 08:19:59 -0400

    update missed Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,8 @@ module.exports = function( grunt ) {
           sass_dir: 'app/styles',
           images_dir: 'app/images',
           javascripts_dir: 'temp/scripts',
-          force: true
+          force: true,
+          output_style: 'compressed'
         }
       }
     },
